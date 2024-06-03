968a36dc7c169233a210d580bf7f4c8937ab4708
Matthew Castiglione <matt3castiglione@sbcglobal.net>
2024-06-03 10:16:15 -0500

    Finished. Correctly prints e to the given digit.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace e_at_Nth_digit;
+
+class Program
+{
+    const double e = Math.E;
+    static void Main(string[] args)
+    {
+        Console.WriteLine(e);
+        Greetings();
+        string? userInput = Console.ReadLine();
+        Console.WriteLine(PrintDigits(userInput));      
+    }
+
+    static void Greetings() 
+    {
+        Console.WriteLine("Hello! Please enter an integer representing what digit position of e you want printed out!: ");
+
+    }
+
+    static double PrintDigits(string userDigit)
+    {    
+        int? digit;
+        double eToDigit;
+
+        if (int.TryParse(userDigit, out int i)){
+            digit = Convert.ToInt32(userDigit);
+
+            eToDigit = Math.Round(e, (int)digit);
+        }
+        else {
+            Console.WriteLine("ERROR: Not an integer");
+            return 0;
+        }
+
+        return eToDigit;
+    }
+}
